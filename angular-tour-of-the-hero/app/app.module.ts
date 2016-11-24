39a99852228dda04af94e30aa199825a4f394cc1
Phong Tran Nam <phong.tran.nam@tpssoft.com.vn>
2016-11-24 16:08:46 +0700

    final routing

--- angular-tour-of-the-hero/app/app.module.ts
+++ angular-tour-of-the-hero/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
-import { HeroesComponent }  from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import {HeroesComponent}  from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service'
+import {DashboardComponent} from "./dashboard.component";
+
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
-  providers: [HeroService],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        HeroesComponent,
+        HeroDetailComponent
+    ],
+    providers: [HeroService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
